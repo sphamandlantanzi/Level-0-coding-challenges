749d02bb7087857cef2fc925998eee3dec475024
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-05-27 19:17:53 +0200

    Delete task 0.5.py

--- task 0.5.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-def area(a, b, c):
-    s = (a + b + c) * 0.5
-    A = (s * (s - a) * (s - b) * (s - c)) ** 0.5
-    return A
-
-print(area(a , b , c))    
