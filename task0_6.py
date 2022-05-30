606e62d2e9dad301db7cdefd6c7c0054272c0555
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-05-30 12:50:10 +0200

    Test case at bottom of progam

--- task0_6.py
+++ task0_6.py
@@ -1,7 +1,3 @@
-number_1 = 3
-number_2 = 99
-number_3 = 46
-
 def maximum(number_1, number_2, number_3):
     if number_1 >= number_2 and number_1 >= number_3:
         return number_1
@@ -10,4 +6,7 @@ def maximum(number_1, number_2, number_3):
     if number_3 >= number_1 and number_3 >= number_2:
         return number_3
 
+number_1 = 3
+number_2 = 99
+number_3 = 46
 print(maximum(number_1, number_1, number_3))          
