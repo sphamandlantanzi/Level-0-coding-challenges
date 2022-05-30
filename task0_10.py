44a5564606e9c6129b061b9e4b557c7e6fcef28c
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-05-30 12:24:52 +0200

    Functionalities inside function, returns not print

--- task0_10.py
+++ task0_10.py
@@ -1,21 +1,23 @@
 
-word_1 = "House".lower()
-word_2 = "Computers".lower()
+word_1 = "House"
+word_2 = "Computers"
 
 def word_match(word_1,word_2):
+    lower_word_1 = word_1.lower()
+    lower_word_2 = word_2.lower()
     saver = []
 
-    for i in range(len(word_1)):
-        for j in range(len(word_2)):
-            if word_1[i] == word_2[j]:
-                saver.append(word_1[i])
+    for i in range(len(lower_word_1)):
+        for j in range(len(lower_word_2)):
+            if lower_word_1[i] == lower_word_2[j]:
+                saver.append(lower_word_1[i])
 
     delete_repeat = set(saver)
     stick = ','.join(delete_repeat)
 
-    ans = print("Common letters: " +stick)
+    print("Common letters: " +stick)
 
-    return ans
+
 
 word_match(word_1,word_2)    
             
