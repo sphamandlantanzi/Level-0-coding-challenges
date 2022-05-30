49bad06c19fa6d7507c64ab1f7334a34e848b5ee
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-05-30 12:58:10 +0200

    Test code at the bottom of program

--- task0_7.py
+++ task0_7.py
@@ -1,14 +1,12 @@
-celcius = 25
 def celcius_to_farenheit(celcius):
     farenheit = (celcius * 9/5) + 32
     return farenheit
    
-print(celcius_to_farenheit(celcius))
-
-
-farenheit = 77
 def farenheit_to_celcius(farenheit):
     celcius = (farenheit - 32) * 5/9
     return celcius
 
+celcius = 25
+farenheit = 77
+print(celcius_to_farenheit(celcius))
 print(farenheit_to_celcius(farenheit))  
