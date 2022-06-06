d91ccdced3fc3ee9e20b9fa03fc791df0d6c75aa
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-06-06 11:06:56 +0200

    Modified task0.6
    Removed lines 9,10 and 11 used the integers as inputs on function call

--- task0_6.py
+++ task0_6.py
@@ -6,7 +6,4 @@ def maximum(number_1, number_2, number_3):
     if number_3 >= number_1 and number_3 >= number_2:
         return number_3
 
-number_1 = 3
-number_2 = 99
-number_3 = 46
-print(maximum(number_1, number_1, number_3))          
+print(maximum(3, 99, 46))          
