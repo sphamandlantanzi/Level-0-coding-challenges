29ec4518b9a446688767326d961507c296835b27
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-06-06 17:12:05 +0200

    Modified task0.8
    Now includes cases where its a minute past the hour

--- task0_8.py
+++ task0_8.py
@@ -2,18 +2,27 @@ def clock(number):
     qoutient   = number // 60
     hour       = number / 60
     min_dec    = hour - qoutient
-    min        = int(min_dec * 60)
-
+    min        = round(min_dec * 60)
+    
     if qoutient == 0 and min == 1:
         return f"{qoutient} hour, {min} minute"
     
     if qoutient == 0:
         return f"{qoutient} hour, {min} minutes"
     
-    if qoutient <= 1:
-        return f"{qoutient} hour, {min} minutes"
+    if qoutient <= 1 and  min == 1:
+        return f"{qoutient} hour, {1} minute"
+
+    if qoutient <= 1 and min > 1:
+        return f"{qoutient} hour, {min} minutes"  
+
+    if qoutient <= 1 and min == 0:
+        return f"{qoutient} hour, {0} minutes"    
        
-    if qoutient > 1:
+    if qoutient > 1 and min != 1:
         return f"{qoutient} hours, {min} minutes"
+
+    if qoutient > 1 and  min == 1:
+        return f"{qoutient} hour, {1} minute"    
     
-print(clock(116))
+print(clock(157))
