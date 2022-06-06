7186beb5008b59aa6ba751dd868ffd447445515f
Sphamandla Sanele Hlongwana <103256286+MrPinkSeat@users.noreply.github.com>
2022-06-06 10:57:57 +0200

    Modified task0.5
    Removed 6,7,8 and using integers as input on the function call

--- task0_5.py
+++ task0_5.py
@@ -3,8 +3,4 @@ def find_area(side_a, side_b, side_c):
     area = (semi_peri * (semi_peri - side_a) * (semi_peri - side_b) * (semi_peri - side_c)) ** 0.5
     return area
 
-side_a = 3
-side_b = 4
-side_c = 5
-
-print(find_area(side_a , side_b , side_c))
+print(find_area(3 , 4 , 5))
